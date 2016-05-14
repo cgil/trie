cc21ebe2d7d713a973fec54fd9cd8f56db8baee8
Carlos Gil <carlos@uber.com>
2016-05-14 15:48:12 +0100

    Adding configuration utils

---
+++ fabfile.py
@@ -0,0 +1,61 @@
+import os
+import sys
+
+from fabric.api import task, local
+from fabric.colors import green, red
+from fabric.context_managers import settings
+
+DEFAULT_ENV = 'development'
+
+
+@task
+def clean():
+    """Remove all .pyc files."""
+    print green('Clean up .pyc files')
+    local("find . -name '*.py[co]' -exec rm -f '{}' ';'")
+
+
+@task
+def shell(env=DEFAULT_ENV):
+    """Run the shell in the environment."""
+    os.environ['CONFIG_ENV'] = './config/%s.yaml' % env
+    local('ipython --ipython-dir ./config/')
+
+
+@task
+def test(args='', env='test'):
+    """Run tests."""
+    os.environ['CONFIG_ENV'] = './config/%s.yaml' % env
+    clean()
+    print green('Running all tests')
+    cmd = ('nosetests -d --verbosity 3 --with-id --nocapture %s' % args)
+
+    with settings(warn_only=True, quiet=True):
+        success = local(cmd).succeeded
+
+    lint()
+
+    if success:
+        print(green('Tests finished running with success.'))
+    else:
+        print(red('Test finished running with errors.'))
+        sys.exit(1)
+
+
+@task
+def lint():
+    """Check for lints"""
+    print green('Checking for lints')
+    return local("flake8 `find . -name '*.py' -not -path '*env/*'` "
+                 "--ignore=E711,E712 --max-line-length=100").succeeded
+
+
+@task
+def bootstrap(env=DEFAULT_ENV):
+    """Bootstrap the environment."""
+    local('mkdir -p logs')
+    print green('\nInstalling requirements')
+    local('pip install -r requirements-test.txt')
+    local('pip install -r requirements-development.txt')
+    local('pip install -r requirements.txt')
+    local('python setup.py develop')
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+__version__ = '0.1'
+
+setup(
+    name='trie',
+    description='Simple mobile stores.',
+    version=__version__,
+    packages=find_packages(),
+)
+
