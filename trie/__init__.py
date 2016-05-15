0022fe9722303493e8b60364a029e5c04f2e2a02
Carlos Gil <carlos@uber.com>
2016-05-15 14:52:55 +0100

    typo

--- trie/__init__.py
+++ trie/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -5,14 +7,15 @@ from trie.utils.configuration import config
 from trie.views.health import health
 from trie.views.home import home
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://{}:{}@{}:{}/{}'.format(
+database_url = os.environ.get('DATABASE_URL') or 'postgres://{}:{}@{}:{}/{}'.format(
     config.get('database.user'),
     config.get('database.password'),
     config.get('database.host'),
     config.get('database.port'),
     config.get('database.name'),
 )
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Set up the database
